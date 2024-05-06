3cacc0e01d923afdf78efe927661b6bb96884a4e
Li Feng <maplestarplay@gmail.com>
2024-05-06 21:56:13 +0800

    complete auth middleware

--- app/admin/contest/internal/server/http.go
+++ app/admin/contest/internal/server/http.go
@@ -1,20 +1,28 @@
 package server
 
 import (
-	v1 "sastoj/api/sastoj/admin/contest/service/v1"
-	"sastoj/app/admin/contest/internal/conf"
-	"sastoj/app/admin/contest/internal/service"
-
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/golang-jwt/jwt/v5"
+	v1 "sastoj/api/sastoj/admin/contest/service/v1"
+	"sastoj/app/admin/contest/internal/conf"
+	"sastoj/app/admin/contest/internal/service"
+	"sastoj/pkg/middleware/auth"
 )
 
 // NewHTTPServer new an HTTP server.
-func NewHTTPServer(c *conf.Server, greeter *service.ContestService, logger log.Logger) *http.Server {
+func NewHTTPServer( d *conf.,c *conf.Server, greeter *service.ContestService, logger log.Logger) *http.Server {
+
+	apiMap := map[string]string{
+		"UpdateContest": "admin",
+	}
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
+			auth.Auth(func(token *jwt.Token) (interface{}, error) {
+				return []byte("sastoj"), nil
+			}, apiMap),
 		),
 	}
 	if c.Http.Network != "" {
--- app/public/auth/internal/biz/auth.go
+++ app/public/auth/internal/biz/auth.go
@@ -6,9 +6,12 @@ import (
 	"encoding/hex"
 	"errors"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/golang-jwt/jwt/v5"
+	"sastoj/app/public/auth/internal/conf"
+	"time"
 )
 
-// Greeter is a Greeter model.
+// User is a Greeter model.
 type User struct {
 	ID        int64
 	Username  string
@@ -18,21 +21,39 @@ type User struct {
 	GroupID   int64
 	GroupName string
 }
+type MyCustomClaims struct {
+	UserId     int64   `json:"user_id"`
+	GroupId    int64   `json:"group_id"`
+	GroupName  string  `json:"group_name"`
+	ContestIds []int64 `json:"contest_ids"`
+	jwt.RegisteredClaims
+}
 
-// GreeterRepo is a Greater repo.
+// AuthRepo is a Greater repo.
 type AuthRepo interface {
 	FindByUserName(context.Context, string) (*User, error)
 }
 
-// GreeterUsecase is a Greeter usecase.
-type AuthUsecase struct {
-	repo AuthRepo
-	log  *log.Helper
+type jwtConfig struct {
+	Secret     string
+	Expiration time.Duration
+	Issuer     string
 }
 
-// NewGreeterUsecase new a Greeter usecase.
-func NewAuthUsecase(repo AuthRepo, logger log.Logger) *AuthUsecase {
-	return &AuthUsecase{repo: repo, log: log.NewHelper(logger)}
+// AuthUsecase is a Greeter usecase.
+type AuthUsecase struct {
+	repo    AuthRepo
+	log     *log.Helper
+	jwtConf jwtConfig
+}
+
+// NewAuthUsecase new a Greeter usecase.
+func NewAuthUsecase(repo AuthRepo, logger log.Logger, c *conf.Jwt) *AuthUsecase {
+	return &AuthUsecase{repo: repo, log: log.NewHelper(logger), jwtConf: jwtConfig{
+		Secret:     c.Secret,
+		Expiration: time.Duration(c.Expiration) * time.Hour,
+		Issuer:     c.Issuer},
+	}
 }
 
 func (uc *AuthUsecase) Login(ctx context.Context, username string, password string) (*User, error) {
@@ -57,4 +78,29 @@ func generateMD5Password(password string, salt string) string {
 
 func verifyPassword(password string, salt string, hash string) bool {
 	return generateMD5Password(password, salt) == hash
+}
+func (uc *AuthUsecase) GenerateJWT(user User, contestIds []int64) (string, error) {
+	// 设置声明（Claims）
+	registeredClaims := jwt.RegisteredClaims{
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(uc.jwtConf.Expiration)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		NotBefore: jwt.NewNumericDate(time.Now()),
+		Issuer:    uc.jwtConf.Issuer,
+		Subject:   "a",
+		ID:        "1",
+	}
+	claims := MyCustomClaims{
+		UserId:           user.ID,
+		GroupId:          user.GroupID,
+		GroupName:        user.GroupName,
+		ContestIds:       contestIds,
+		RegisteredClaims: registeredClaims,
+	}
+	// 使用密钥对 Token 进行签名，生成最终的 JWT
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(uc.jwtConf.Secret))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
 }
