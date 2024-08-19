0a26c13a5760a526de8bbf8ea93503ab11487a06
Love98 <git@love98.net>
2024-08-19 11:45:43 +0800

    feat: support rate limiter for heavy api(submit and self-test)

--- app/user/contest/internal/server/server.go
+++ app/user/contest/internal/server/server.go
@@ -1,8 +1,16 @@
 package server
 
 import (
+	v1 "sastoj/api/sastoj/user/contest/service/v1"
+	"time"
+
 	"github.com/google/wire"
 )
 
 // ProviderSet is server providers.
 var ProviderSet = wire.NewSet(NewGRPCServer, NewHTTPServer)
+
+var apiLimiter = map[string]time.Duration{
+	v1.Contest_Submit_FullMethodName:   10 * time.Second,
+	v1.Contest_SelfTest_FullMethodName: 5 * time.Second,
+}
