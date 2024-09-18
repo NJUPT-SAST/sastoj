e865ba9ce7bb89c0b9af4bb6d13f6c96b0fa71f1
Aiden2014 <HiAidenLiu@gmail.com>
2024-09-18 21:15:03 +0800

    fix: save and get correct contest state.

--- app/admin/contest/internal/data/contest.go
+++ app/admin/contest/internal/data/contest.go
@@ -3,6 +3,7 @@ package data
 import (
 	"context"
 	"errors"
+	v1 "sastoj/api/sastoj/admin/contest/service/v1"
 	"sastoj/app/admin/contest/internal/biz"
 	"sastoj/ent/contest"
 	"time"
@@ -23,10 +24,14 @@ func NewContestRepo(data *Data, logger log.Logger) biz.ContestRepo {
 }
 
 func (r *ContestRepo) Save(ctx context.Context, g *biz.Contest) (*biz.Contest, error) {
+	entState, err := stateToEntState(g.Status)
+	if err != nil {
+		return nil, err
+	}
 	res, err := r.data.db.Contest.Create().
 		SetTitle(g.Title).
 		SetDescription(g.Description).
-		SetState(contest.State(g.Status)).
+		SetState(entState).
 		SetType(int16(g.Type)).
 		SetStartTime(g.StartTime).
 		SetEndTime(g.EndTime).
@@ -42,10 +47,14 @@ func (r *ContestRepo) Save(ctx context.Context, g *biz.Contest) (*biz.Contest, e
 }
 
 func (r *ContestRepo) Update(ctx context.Context, g *biz.Contest) error {
-	_, err := r.data.db.Contest.Update().
+	entState, err := stateToEntState(g.Status)
+	if err != nil {
+		return err
+	}
+	_, err = r.data.db.Contest.Update().
 		SetTitle(g.Title).
 		SetDescription(g.Description).
-		SetState(contest.State(g.Status)).
+		SetState(entState).
 		SetType(int16(g.Type)).
 		SetStartTime(g.StartTime).
 		SetEndTime(g.EndTime).
@@ -70,7 +79,7 @@ func (r *ContestRepo) FindByID(ctx context.Context, id int64) (*biz.Contest, err
 		Id:          po.ID,
 		Title:       po.Title,
 		Description: po.Description,
-		Status:      stateMap(po.State, po.StartTime, po.EndTime),
+		Status:      stateToInt(po.State, po.StartTime, po.EndTime),
 		Type:        int32(po.Type),
 		StartTime:   po.StartTime,
 		EndTime:     po.EndTime,
@@ -96,9 +105,10 @@ func (r *ContestRepo) ListPages(ctx context.Context, current int64, size int64) 
 	rv := make([]*biz.Contest, 0)
 	for _, po := range res {
 		rv = append(rv, &biz.Contest{
+			Id:          po.ID,
 			Title:       po.Title,
 			Description: po.Description,
-			Status:      stateMap(po.State, po.StartTime, po.EndTime),
+			Status:      stateToInt(po.State, po.StartTime, po.EndTime),
 			Type:        int32(po.Type),
 			StartTime:   po.StartTime,
 			EndTime:     po.EndTime,
@@ -144,7 +154,7 @@ func (r *ContestRepo) GetRacingContests(ctx context.Context) ([]*biz.Contest, er
 		rv = append(rv, &biz.Contest{
 			Title:       c.Title,
 			Description: c.Description,
-			Status:      stateMap(c.State, c.StartTime, c.EndTime),
+			Status:      stateToInt(c.State, c.StartTime, c.EndTime),
 			Type:        int32(c.Type),
 			StartTime:   c.StartTime,
 			EndTime:     c.EndTime,
@@ -156,14 +166,16 @@ func (r *ContestRepo) GetRacingContests(ctx context.Context) ([]*biz.Contest, er
 	return rv, nil
 }
 
-func stateMap(state contest.State, start, end time.Time) int32 {
+func stateToInt(state contest.State, start, end time.Time) int32 {
 	switch state {
 	case contest.StateNORMAL:
-		if time.Now().Before(start) {
+		now := time.Now()
+		switch {
+		case now.Before(start):
 			return 0
-		} else if time.Now().After(end) {
+		case now.After(end):
 			return 2
-		} else {
+		default:
 			return 1
 		}
 	case contest.StateCANCELLED:
@@ -174,5 +186,20 @@ func stateMap(state contest.State, start, end time.Time) int32 {
 		return 5
 	default:
 		return -1
+	}
+}
+
+func stateToEntState(state int32) (contest.State, error) {
+	switch state {
+	case 0, 1, 2:
+		return contest.StateNORMAL, nil
+	case 3:
+		return contest.StateCANCELLED, nil
+	case 4:
+		return contest.StateHIDDEN, nil
+	case 5:
+		return contest.StateDELETED, nil
+	default:
+		return "", v1.ErrorContestInvalid("contest type invalid")
 	}
 }
