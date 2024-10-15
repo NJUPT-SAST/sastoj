6d578532f865695895675225d22487abdc34c031
Dylan Deng <ballade0d@gmail.com>
2024-10-15 12:44:22 +0800

    fix: update gateway dto

--- app/user/gateway/internal/service/problem.go
+++ app/user/gateway/internal/service/problem.go
@@ -14,8 +14,9 @@ func (s *GatewayService) GetProblems(ctx context.Context, req *pb.GetProblemsReq
 	for _, p := range rv {
 		reply.Problems = append(reply.Problems, &pb.GetProblemsReply_Problem{
 			Id:    p.ID,
+			Type:  p.Type,
 			Title: p.Title,
-			Score: int32(p.Point),
+			Score: p.Score,
 			Index: int32(p.Index),
 		})
 	}
@@ -27,9 +28,11 @@ func (s *GatewayService) GetProblem(ctx context.Context, req *pb.GetProblemReque
 		return nil, err
 	}
 	return &pb.GetProblemReply{
-		Id:      rv.ID,
-		Title:   rv.Title,
-		Content: rv.Content,
-		Score:   int32(rv.Point),
+		Id:       rv.ID,
+		Type:     rv.Type,
+		Title:    rv.Title,
+		Content:  rv.Content,
+		Score:    rv.Score,
+		Metadata: rv.Metadata,
 	}, nil
 }
